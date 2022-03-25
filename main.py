8d903c59bb84b73d65e9e1c78589b7fce7752277
Lautaro Perez Carmona <lautaropc2001@gmail.com>
2022-03-25 10:33:30 -0300

    New format to calculate times

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 
 
 if __name__ == '__main__':
-    length = 100000
+    length = int(input('Put the length of the list >> '))
     sorted_list = set()
     for n in range(length):
         sorted_list.add( random.randint( -3*length, 3*length ) )
@@ -14,24 +14,36 @@ if __name__ == '__main__':
     sorted_list = sorted( list(sorted_list) )
 
     f = open('results.txt', 'a')
+    sequential_time = 0
+    binary_time = 0
+    rec_binary_time = 0
 
-    start = time.time()
-    for target in sorted_list:
-        sequential_search(sorted_list, target)
-    end = time.time()
-    f.write(f'Sequential Search       -> {(start+end) / 2} seconds \n')
+    for i in range(length):
+        target = random.choice( sorted_list )
+        start = time.time()
+        sequential_search(sorted_list,target)
+        end = time.time()
+        sequential_time += (start+end) / 2
 
-    start = time.time()
-    for target in sorted_list:
-        recursive_binary_search(sorted_list, target)
-    end = time.time()
-    f.write(f'(Recursive)Binary Search-> {(start+end) / 2} seconds \n')
+        start = time.time()
+        binary_search(sorted_list,target)
+        end = time.time()
+        binary_time += (start+end) / 2
 
-    start = time.time()
-    for target in sorted_list:
-        binary_search(sorted_list, target)
-    end = time.time()
-    f.write(f'Binary Search           -> {(start+end) / 2} seconds \n\n')
+        start = time.time()
+        recursive_binary_search(sorted_list,target)
+        end = time.time()
+        rec_binary_time += (start+end) / 2
+        
+
+
+    f.write(f'Sequential Search       -> {sequential_time} seconds \n')
+
+    f.write(f'(Recursive)Binary Search-> {binary_time} seconds \n')
+
+    f.write(f'Binary Search           -> {rec_binary_time} seconds \n\n')
+
+    f.close()
 
 
 
